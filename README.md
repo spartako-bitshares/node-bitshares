aa6e13e18dfdf426938183487e828fd48b3d64b3
Fabio Castaldo <fabiocastaldo@gmail.com>
2014-12-24 22:13:02 +0100

    Dynamic method and examples

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 node-bitshares
 ==============
 
-API wrapper for bitshares toolkit in nodejs 
+API wrapper for bitshares toolkit in nodejs.
+
+Start the bitshares_client using this command:
+
+```
+$ ./bitshares_client  --rpcuser user  --rpcpassword pass --server --httpport 4000
+```
+
+the createClient method create the rpc client
+```js
+var bitshares = require('bitshares');
+
+bitshares.createClient("user","pass","127.0.0.1",4000,function(err,client){
+	if(err){return console.log(err);};
+	// You can call every method from help command, the first arg is the params list
+	client.get_info([],function(err,res){
+		if(err){return console.log(err);};
+		console.log(res);
+	});
+});
+
+```
+
+See examples/ for other commands usage.
+
---
+++ examples/main.js
@@ -0,0 +1,42 @@
+var bitshares = require("../"),
+        async = require('async'),
+     minimist = require('minimist');
+
+var main = function(){
+  var argv = minimist(process.argv.slice(2));
+  var client = null;
+  // Waterfall operations example
+  async.waterfall([
+    function(cb){
+      bitshares.createClient(argv.rpcuser,argv.rpcpassword,argv.host||"127.0.0.1",argv.httpport,cb);
+    },
+    function(_client, cb){
+      client = _client;
+      cb(false,{});
+    },      
+    function(res, cb){
+      client.get_info([],cb);
+    },      
+    function(res, cb){
+      console.log("get_info",res);
+      client.wallet_open(["default"],cb);
+    },
+    function(res, cb){
+      console.log("open",res);
+      client.wallet_account_balance([],cb);
+    },
+    function(res, cb){
+      console.log("balance",res);
+      cb(false,"end");
+    },
+  ], function (err, res) {
+    if(err){
+      console.log("err",err);
+    }
+    else{
+      console.log(res);
+    }
+  });
+};
+
+main();
